be8ad3f44339d75cc1bbb541fbe12a56f7dc0d73
vivek-goyal12 <vivek.goyal_cs18@gla.ac.in>
2019-08-24 23:08:01 +0530

    initial commit @ author vivek goyal first we declare a variable name 't'to take the input from user after this we take a new variable of double data type name 'speed' to find speed then distance is calculated by the shown formula in code and in the last we print the output

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,13 @@ public class Main {
 
         public static void main(String[] args) {
             Scanner scan  = new Scanner(System.in);
+            double t = scan.nextDouble();
+            scan.close();
+            double g = 9.8;
+            double speed = g*t;
+            double distance =(1f/2f)*( g * t *t);
+            System.out.println("the speed of object at " + t + " seconds after its release is" + speed + " and the distance travelled by the object in the " + t + " seconds after the release is "+ distance);
+
 
 
         }}
